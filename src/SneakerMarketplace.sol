f09c9d85db06b14412341c4672714abd23c2eb05
Ridhit Garg <ridhitgarg@ucsb.edu>
2024-06-12 17:17:34 -0700

    added contract functions

--- src/SneakerMarketplace.sol
+++ src/SneakerMarketplace.sol
@@ -14,26 +14,30 @@ contract SneakerMarketplace {
     mapping(address => uint8) private roles;
     mapping(address => uint256) private balances;
     
-    // mapp address to the sneaker id
-    // track the exisiting listings
+    // map address to the sneaker id
+    // track the existing listings
     mapping(address => uint256) private listings;
 
+    mapping(uint256 => address) private sneakerToSeller;
+    mapping(uint256 => uint256) private sneakerPrices;
+
+    // mapping from sneaker ID to the address of the buyer
+    mapping(uint256 => address) private sneakerBuyers;
 
     modifier onlyAdmin() {
         require(msg.sender == admin, "Only admin can perform this action");
         _;
     }
 
-    modifier onlySeller(uint256 sneakerId) {
+    modifier onlySeller() {
         require(roles[msg.sender] == 2, "Only the seller can perform this action");
         _;
     }
 
-    modifier onlyBuyer(uint256 sneakerId) {
+    modifier onlyBuyer() {
         require(roles[msg.sender] == 3, "Only the buyer can perform this action");
         _;
     }
-
 
     constructor() {
         // setting the admin as the deployer of the contract
@@ -41,7 +45,6 @@ contract SneakerMarketplace {
     }
 
     // register a new user
-
     function registerBuyer() public returns (bool) {
         // check if user is unregistered
         if (roles[msg.sender] == 0) {
@@ -56,7 +59,7 @@ contract SneakerMarketplace {
     // Register the caller as seller
     function registerSeller() public returns (bool) {
         if (roles[msg.sender] == 0) {
-            // make user a guest
+            // make user a seller
             roles[msg.sender] = 2;
             return true;
         } else {
@@ -65,31 +68,66 @@ contract SneakerMarketplace {
     }
 
     // function for buyers
-
     // when buyer buys a sneaker
-    // should transfer money to DeSneaker
-    function buySneaker(uint256 sneakerId) public payable onlyBuyer(sneakerId) {
+    // should transfer money to the contract
+    function buySneaker(uint256 sneakerId) public payable onlyBuyer {
+        address seller = getSellerBySneakerId(sneakerId);
+        require(seller != address(0), "Sneaker not listed for sale");
+        require(msg.value > 0, "Value must be greater than zero");
+        //require(msg.value == sneakerPrices[sneakerId], "Incorrect price");
+
+        // Record the buyer
+        sneakerBuyers[sneakerId] = msg.sender;
+
+        // Transfer money to the contract
+        balances[address(this)] += msg.value;
     }
 
     // after buying, buyer will be able to confirm delivery
-    // this wil then ask the vault (smart contract) to withdraw the funds to the seller
-    function confirmDelivery(uint256 sneakerId) public onlyBuyer(sneakerId) {
+    // this will then transfer the funds to the seller
+    function confirmDelivery(uint256 sneakerId) public onlyBuyer {
+        require(sneakerBuyers[sneakerId] == msg.sender, "You are not the buyer of this sneaker");
+
+        address seller = getSellerBySneakerId(sneakerId);
+        require(seller != address(0), "Seller not found");
+
+        // Transfer the funds to the seller
+        uint256 amount = balances[address(this)];
+        require(amount > 0, "No funds to transfer");
+
+        balances[seller] = 0;
+        payable(seller).transfer(amount);
     }
 
+    // If the buyer doesn't receive the item, this function refunds the money
+    function noDelivery(uint256 sneakerId) public onlyBuyer {
+        require(sneakerBuyers[sneakerId] == msg.sender, "You are not the buyer of this sneaker");
 
+        uint256 amount = balances[address(this)];
+        //require(amount > 0, "No funds to refund");
+
+        balances[address(this)] -= amount;
+
+        payable(msg.sender).transfer(amount);
+    }
 
     // functions for the seller
-
     // list a sneaker
-    function listSneaker(uint256 sneakerId) public payable onlySeller(sneakerId) {
+    function listSneaker(uint256 sneakerId) public onlySeller {
         // make sure seller doesn't already have a listing
         require(listings[msg.sender] == 0, "Already have a listing");
         listings[msg.sender] = sneakerId;
+        sneakerToSeller[sneakerId] = msg.sender;
+        //sneakerPrices[sneakerId] = price;
     }
 
     // take the listing down
-    function withdrawListing() public onlySeller(listings[msg.sender]) {
+    function withdrawListing() public onlySeller {
+        uint256 sneakerId = listings[msg.sender];
+        require(sneakerId != 0, "No listing to withdraw");
+
         listings[msg.sender] = 0;
+        sneakerToSeller[sneakerId] = address(0);
     }
 
     // getters
@@ -98,14 +136,9 @@ contract SneakerMarketplace {
         return sneakerId;
     }
 
-    function withdrawFunds() public {
-        uint256 amount = balances[msg.sender];
-        require(amount > 0, "No funds to withdraw");
 
-        balances[msg.sender] = 0;
-        payable(msg.sender).transfer(amount);
-
+    // helper function to find the seller by sneaker ID
+    function getSellerBySneakerId(uint256 sneakerId) private view returns (address seller) {
+        return sneakerToSeller[sneakerId];
     }
-
-
 }
